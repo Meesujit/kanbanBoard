81026b95b52e48f5ac2012dcd90ab060f02a4f25
sujit gouda <sujit.gda997@gmail.com>
2024-11-05 13:07:45 +0530

    removed file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import DemoBoard from './components/demo'
-// import KanbanBoard from './components/KanbanBoardVo'
-// import KanbanBoard from './components/KanbanBoard'
+
 
 const App = () => {
   return (
     <>
-    {/* <KanbanBoard /> */}
-    {/* <KanbanBoard /> */}
+
     <DemoBoard />
     
     </>
